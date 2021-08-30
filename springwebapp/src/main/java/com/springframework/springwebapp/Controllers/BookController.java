45c3afaf4eb55caa3fdecdaeb2dfb2048d40765d
bwaterman8 <bwaterman8@gmail.com>
2021-08-29 20:50:57 -0400

    made book controller

--- springwebapp/src/main/java/com/springframework/springwebapp/Controllers/BookController.java
+++ springwebapp/src/main/java/com/springframework/springwebapp/Controllers/BookController.java
@@ -1,4 +1,25 @@
 package com.springframework.springwebapp.Controllers;
 
+import com.springframework.springwebapp.Repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+
 }
